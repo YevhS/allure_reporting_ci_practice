809f732db2bf55a95d05bc2c058b1e0e52e5ca8e
Yevhen Stepanenko <yevhen.s@mate.academy>
2023-04-26 11:33:45 +0300

    new commit

--- upload_test_results.sh
+++ upload_test_results.sh
@@ -2,5 +2,5 @@
 
 # Check if we should upload to Allure TestOps based on the build ID
 if bash -c '[[ "$CODEBUILD_BUILD_ID" = "cusbo-build-project:"* ]]'; then
-  ./allurectl upload --launch-name "New commit" ./api/target/allure-results
+  ./allurectl upload --launch-name "New commit" ./target/allure-results
 fi
