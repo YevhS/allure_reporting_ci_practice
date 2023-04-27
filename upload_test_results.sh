00683e4a3459899fe0edc13403db149f54adc000
Yevhen Stepanenko <yevhen.s@mate.academy>
2023-04-27 20:35:45 +0300

    fixed path to sh file

--- upload_test_results.sh
+++ upload_test_results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Check if we should upload to Allure TestOps based on the build ID
-if [[ "$CODEBUILD_BUILD_ID" = "cusbo-build-project:"* ]]; then
+if [[ "$CODEBUILD_BUILD_ID" = "cusbo-build-project:"* && -d "./target/allure-results" ]]; then
   ./allurectl upload --launch-name "New commit" ./target/allure-results
 fi
