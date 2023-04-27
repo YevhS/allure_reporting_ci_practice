fe3bb9001a3c0e51b8e9b270976aecd23b27f943
Yevhen Stepanenko <yevhen.s@mate.academy>
2023-04-27 14:16:35 +0300

    changed buildspec file

--- upload_test_results.sh
+++ upload_test_results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Check if we should upload to Allure TestOps based on the build ID
-if bash -c '[[ "$CODEBUILD_BUILD_ID" = "cusbo-build-project:"* ]]'; then
+if [[ "$CODEBUILD_BUILD_ID" = "cusbo-build-project:"* ]]; then
   ./allurectl upload --launch-name "New commit" ./target/allure-results
 fi
